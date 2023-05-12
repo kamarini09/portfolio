31c95e664162d67ec1d07f4e16a0c3f386460ffe
kamarini <kama0549@stud.kea.dk>
2023-05-12 12:48:11 +0200

    delete comments

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,13 +4,13 @@ import Link from 'next/link';
 export default function Contact() {
     return (
         <div id='contact' className={styles.container}>
-          <Link href="https://github.com/kamarini09">
+          <Link href="https://github.com/kamarini09" target="_blank">
             <button className={styles.button}>GitHub</button>
           </Link>
-          <Link href="https://www.instagram.com/kamarini._/">
+          <Link href="https://www.instagram.com/kamarini._/" target="_blank">
             <button className={styles.button}>Instagram</button>
           </Link>
-          <Link href="https://www.linkedin.com/in/kamarini-moragianni/">
+          <Link href="https://www.linkedin.com/in/kamarini-moragianni/" target="_blank">
             <button className={styles.button}>LinkedIn</button>
           </Link>
           <Link href="/CV.pdf" target="_blank">
