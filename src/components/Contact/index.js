33d819e3e39b9a9e209a6b05d58fb6fc53197fa1
kamarini <kama0549@stud.kea.dk>
2023-05-14 12:15:44 +0200

    re-order the buttons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,18 +4,20 @@ import Link from 'next/link';
 export default function Contact() {
     return (
         <div id='contact' className={styles.container}>
+          <Link href="/CV.pdf" target="_blank">
+             <button className={styles.button} download>CV</button>
+          </Link>
           <Link href="https://github.com/kamarini09" target="_blank">
             <button className={styles.button}>GitHub</button>
+          </Link>
+          
+          <Link href="https://www.linkedin.com/in/kamarini-moragianni/" target="_blank">
+            <button className={styles.button}>LinkedIn</button>
           </Link>
           <Link href="https://www.instagram.com/kamarini._/" target="_blank">
             <button className={styles.button}>Instagram</button>
           </Link>
-          <Link href="https://www.linkedin.com/in/kamarini-moragianni/" target="_blank">
-            <button className={styles.button}>LinkedIn</button>
-          </Link>
-          <Link href="/CV.pdf" target="_blank">
-             <button className={styles.button} download>CV</button>
-          </Link>
+          
         </div>
       );
 }
